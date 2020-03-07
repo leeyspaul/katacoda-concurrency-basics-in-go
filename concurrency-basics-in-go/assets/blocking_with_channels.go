f8bf4a098f0dab95b2724e004309d66c44644c52
leeyspaul <t.leeyspaul@gmail.com>
2020-03-07 15:16:10 -0800

    add blocking channels/wg examples

---
+++ concurrency-basics-in-go/assets/blocking_with_channels.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var paul User
+
+	paul.UserName = "Paul"
+	paul.UserId = 137
+
+	paul = updateUserId(paul, 200)
+
+	fmt.Printf("id update for user paul is %d\n", paul.UserId)
+}
+
+type User struct {
+	UserName string
+	UserId int
+}
+
+func updateUserId(user User, userId int) (updatedUser User) {
+
+	go func() {
+		user.UserId = userId
+	}()
+
+	return user
+}
+
+func updateUserIdFixed(user User, userId int) (updatedUser User) {
+
+	// ...to be filled!
+	return
+}
